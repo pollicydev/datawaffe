e2e99ac2815b53ccb5361d1e57042650b6587ece
Collin Rukundo <collin@matchstick.ug>
2022-10-04 15:16:00 +0300

    update

--- config/settings/production.py
+++ config/settings/production.py
@@ -15,7 +15,7 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list(
     "DJANGO_ALLOWED_HOSTS",
-    default=[".pollicy.org, localhost, 147.182.178.237, .matchstick.ug"],
+    default=["*"],
 )
 
 # DATABASES
