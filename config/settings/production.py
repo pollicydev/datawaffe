4771ec71561045d44367ef8e2b56b296cff73d99
Collin Rukundo <collin@matchstick.ug>
2022-10-04 15:10:53 +0300

    updated allowed_hosts

--- config/settings/production.py
+++ config/settings/production.py
@@ -13,7 +13,10 @@ from .base import env
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["rrap.pollicy.org"])
+ALLOWED_HOSTS = env.list(
+    "DJANGO_ALLOWED_HOSTS",
+    default=[".pollicy.org, localhost, 147.182.178.237, .matchstick.ug"],
+)
 
 # DATABASES
 # ------------------------------------------------------------------------------
