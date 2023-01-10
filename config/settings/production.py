868ed4dddaa71ea2e346e1f00b4da63a514a86ba
Collin Rukundo <collin@matchstick.ug>
2023-01-10 14:52:26 +0300

    Update

--- config/settings/production.py
+++ config/settings/production.py
@@ -96,7 +96,9 @@ AWS_S3_CUSTOM_DOMAIN = env("DJANGO_AWS_S3_CUSTOM_DOMAIN", default=None)
 aws_s3_domain = AWS_S3_CUSTOM_DOMAIN or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 # STATIC
 # ------------------------
+
 STATICFILES_STORAGE = "rrap.utils.storages.StaticRootS3Boto3Storage"
+STATICFILES_LOCATION = "static"
 COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
 AWS_PRELOAD_METADATA = True
 COLLECTFAST_THREADS = 20
@@ -106,6 +108,7 @@ STATIC_URL = f"https://{aws_s3_domain}/static/"
 # MEDIA
 # ------------------------------------------------------------------------------
 DEFAULT_FILE_STORAGE = "rrap.utils.storages.MediaRootS3Boto3Storage"
+MEDIAFILES_LOCATION = "media"
 MEDIA_URL = f"https://{aws_s3_domain}/media/"
 
 # EMAIL
--- rrap/utils/storages.py
+++ rrap/utils/storages.py
@@ -1,11 +1,12 @@
 from storages.backends.s3boto3 import S3Boto3Storage
+from django.conf import settings
 
 
 class StaticRootS3Boto3Storage(S3Boto3Storage):
-    location = "static"
+    location = settings.STATICFILES_LOCATION
     default_acl = "public-read"
 
 
 class MediaRootS3Boto3Storage(S3Boto3Storage):
-    location = "media"
+    location = settings.MEDIAFILES_LOCATION
     file_overwrite = False
